819d8e0f122e22cbd2bd28afdebf440b71ee14e6
novakdr <novakdr23@gmail.com>
2020-05-05 12:12:35 -0700

    Added NASA API Fetch and DOM population
    We getting a little bit fancier in here

--- script.js
+++ script.js
@@ -2,48 +2,54 @@ const imgContainer = document.getElementById("img-container");
 const overlay = document.getElementById("overlay");
 const overlayImage = overlay.querySelector("img");
 const overlayClose = document.getElementById("close");
-const pictureArray = ["https://apod.nasa.gov/apod/image/2002/Eta-HST-ESO-New-LL1024.jpg", "https://apod.nasa.gov/apod/image/1910/HorseFlame_Priego_3252.jpg", "https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_960.jpg", "https://apod.nasa.gov/apod/image/2002/Eta-HST-ESO-New-LL1024.jpg", "https://apod.nasa.gov/apod/image/1910/HorseFlame_Priego_3252.jpg", "https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_960.jpg", "https://apod.nasa.gov/apod/image/2002/Eta-HST-ESO-New-LL1024.jpg", "https://apod.nasa.gov/apod/image/1910/HorseFlame_Priego_3252.jpg", "https://apod.nasa.gov/apod/image/1909/HeartNebula_Falls_960.jpg"];
 
-// POPULATE IMAGE GALLERY WITH, WELL, IMAGES
-pictureArray.forEach((e, val) => {
-    const img = document.createElement("div");
-    img.classList.add("img");
+// FETCHING STUFF AND POPULATING DOM
+fetch('https://images-api.nasa.gov/search?q=nebula')
+    .then(res => res.json())
+    .then(data => {
+        const dataArray = data.collection.items;
 
-    if (val % 2 === 0) {
-        img.classList.add("v2", "h4");
-    } else {
-        img.classList.add("v4", "h4");
-    }
+        dataArray.forEach((e, val) => {
 
-    img.innerHTML = `
-        <img src="${e}" alt="a gorgeous picture of a nebula">
-        <div class="img-overlay">
-            <button id="btn">View</button>
-        </div>
-    `;
+            const img = document.createElement("div");
+            img.classList.add("img");
 
-    imgContainer.appendChild(img);
+            if (val % 2 === 0) {
+                img.classList.add("v2", "h4");
+            } else {
+                img.classList.add("v4", "h4");
+            }
+
+            e.links.forEach((e, val) => {
+
+                img.innerHTML = `
+                    <img src="${e.href}" alt="a gorgeous picture of a nebula">
+                    <div class="img-overlay">
+                        <button id="btn">View</button>
+                    </div>
+                `;
+
+                imgContainer.appendChild(img);
+            });
+        });
+
+        // EVENT LISTENERS
+        function handleClick(event) {
+            const imageSrc = event.currentTarget.querySelector('img').src;
+            overlayImage.src = imageSrc;
+            overlay.classList.add('open');
+        }
+
+        function close() {
+            overlay.classList.remove("open");
+        }
+
+        const images = document.querySelectorAll(".img");
+
+        images.forEach(image => image.addEventListener("click", handleClick));
+        overlayClose.addEventListener("click", close);
+    });
+
+window.addEventListener('click', (event) => {
+    event.target == overlay ? overlay.classList.remove('open') : false
 });
-
-// EVENT LISTENERS
-function handleClick(e) {
-    const imageSrc = e.currentTarget.querySelector('img').src;
-    console.log(imageSrc);
-    overlayImage.src = imageSrc;
-
-    overlay.classList.add('open');
-    overlay.classList.add('open');
-}
-
-function close() {
-    overlay.classList.remove("open");
-}
-
-const images = document.querySelectorAll(".img");
-
-images.forEach(image => image.addEventListener("click", handleClick));
-overlayClose.addEventListener("click", close);
-
-window.addEventListener('click', (e) => {
-    e.target == overlay ? overlay.classList.remove('open') : false
-})
